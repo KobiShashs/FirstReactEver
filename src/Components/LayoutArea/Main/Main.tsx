b23db904b159a976e6a6c79c298a4d5506a65501
Kobi Shasha <kobi.shasha@gmail.com>
2021-07-11 21:51:14 +0300

    State, Props, Two Way Binding and lifecycle

--- src/Components/LayoutArea/Main/Main.tsx
+++ src/Components/LayoutArea/Main/Main.tsx
@@ -5,26 +5,44 @@ import Workout from "../../MainArea/Workout/Workout";
 import Clubs from "../../MainArea/Clubs/Clubs";
 import Goals from "../../MainArea/Goals/Goals";
 import Assists from "../../MainArea/Assists/Assists";
+import MakeSound from "../../MainArea/MakeSound/MakeSound";
+import BestTeam from "../../MainArea/BestTeam/BestTeam";
+import Search from "../../MainArea/Search/Search";
+import Clock from "../../MainArea/Clock/Clock";
 function Main(): JSX.Element {
     // const url = 'https://media.giphy.com/media/loG32MXYyQl5vUJL46/giphy.gif';
     return (
         <div className="Main">
 
+
             {/* Interpolation */}
-            <Total/>
+            {/* <Total/> */}
 
             {/* Conditional Rendering */}
-            <Workout/>
+            {/* <Workout/> */}
 
             {/* Displaying List */}
-            <Clubs/>
+            {/* <Clubs/> */}
 
             {/* Props */}
-            <Goals team='FCB' numOfGoals={700}/>
+            {/* <Goals team='FCB' numOfGoals={700}/> */}
             {/* Optional & Short Circuit */}
-            <Goals  numOfGoals={748}/>
+            {/* <Goals  numOfGoals={748}/> */}
 
-            <Assists team='FCB' numOfAssists={500}/>
+            {/* Props */}
+            {/* <Assists team='FCB' numOfAssists={500}/> */}
+
+            {/* Events */}
+            {/* <MakeSound/> */}
+
+            {/* Events & State */}
+            {/* <BestTeam/> */}
+
+            {/* Two way Binding */}
+            <Search/>
+
+            {/* Life cycle Hooks */}
+            <Clock/>
 
         </div>
     );
